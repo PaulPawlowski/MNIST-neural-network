487770dbc5c971b1cc98a709533f7be4c28a1ef5
PaulPawlowski <100875666+PaulPawlowski@users.noreply.github.com>
2023-06-20 15:00:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,37 +6,28 @@ using namespace std;
 int main(void){
     
     //----------------------------------------------------------------------------------------------------------------------------------------//
-    // LOAD AND ORGANIZE DATA
-    string IDXIMAGES = "/Users/paulpawlowski/Documents/C++/nn2/data/train-images-idx3-ubyte";   // SPECIFY IMAGES IDX-FILE PATH
-    string IDXLABELS = "/Users/paulpawlowski/Documents/C++/nn2/data/train-labels-idx1-ubyte";   // SPECIFY LABELS IDX-FILE PATH
-    vector<arma::vec>                                   images;
-    vector<int>                                         labels;
+    // LOAD DATA                                                FILEPATHS FOR:
+    string IDXTRAINIMAGES {".../train-images-idx3-ubyte"};   // TRAINING IMAGES
+    string IDXTRAINLABELS {".../train-labels-idx1-ubyte"};   // TRAINING LABELS
+    string IDXTESTIMAGES  {".../t10k-images-idx3-ubyte"};    // TESTING IMAGES
+    string IDXTESTLABELS  {".../t10k-labels-idx1-ubyte"};    // TESTING LABELS
+    
     vector<arma::vec>                                   trainimages;
     vector<int>                                         trainlabels;
     vector<arma::vec>                                   testimages;
     vector<int>                                         testlabels;
+    read_mnist_cv(IDXTRAINIMAGES.c_str(), IDXTRAINLABELS.c_str(), trainimages, trainlabels);
+    read_mnist_cv(IDXTESTIMAGES.c_str(),  IDXTESTLABELS.c_str(),  testimages,  testlabels);
     
-    read_mnist_cv(IDXIMAGES.c_str(), IDXLABELS.c_str(), images, labels);
-    
-    for(int i = 0; i < 50000; i++){
-        trainimages.push_back(images.at(i));
-        trainlabels.push_back(labels.at(i));
-    }
-    for(int i = 50000; i < 60000; i++){
-        testimages.push_back(images.at(i));
-        testlabels.push_back(labels.at(i));
-    }
-    images.clear();
-    labels.clear();
     
     //----------------------------------------------------------------------------------------------------------------------------------------//
     // INSTANTIATE NETWORK
-    vector<int> NETWORK_DIMENSIONS      {28*28, 30, 10};                                        // SPECIFY NETWORK DIMENSIONS
+    vector<int> NETWORK_DIMENSIONS      {28*28, 30, 10};    // SPECIFY NETWORK DIMENSIONS
     network                             network(NETWORK_DIMENSIONS);
     
     //----------------------------------------------------------------------------------------------------------------------------------------//
     // TRAIN AND MEASURE ACCURACY
-    int EPOCHS = 100;                                                                           // SPECIFY NUMBER OF TRAINING EPOCHS
+    int EPOCHS = 100;    // SPECIFY NUMBER OF TRAINING EPOCHS
     for(int e = 0; e < EPOCHS; e++){
         cout << endl << "epoch: " << e+1 << endl;
         network.stochastic_gradient_descent(trainimages, trainlabels, 0.3, 10);
@@ -46,5 +37,3 @@ int main(void){
     
     return 0;
 }
-
-
